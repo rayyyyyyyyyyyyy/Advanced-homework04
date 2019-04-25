be84a58ef25a09de3af3d2b29bf4dd885e8666ab
rayyyyyyyyyyyyy <48341285+rayyyyyyyyyyyyy@users.noreply.github.com>
2019-04-25 20:50:08 +0800

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include<stdio.h>
+int main()
+{
+int days[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int i,j,k,y,m,d,b,n,sum;
+while(scanf("%d/%d/%d",&y,&m,&d)!=EOF)/*EX:2019/1/1*/
+{
+sum=0;
+for(i=1;i<m;i++)
+sum=sum+days[i];
+sum=sum+d;
+if((y%4==0||(y%400==0&&y%100!=0)) &&m>2)
+sum=sum+1;
+printf("²Ä%d¤Ñ\n",sum);
+}
+}
